5e4f41690a0be0663b4eb1d68c05a8d35593b7d0
rajeevthapa09 <rajeevthapa09@gmail.com>
2023-10-20 23:41:20 -0500

    second update after first screw ups

--- frontend/App.js
+++ frontend/App.js
@@ -1,12 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import React, { useEffect } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useState } from 'react';
+import { MaterialCommunityIcons } from 'react-native-vector-icons';
+import GlobalContext from "./context"
+import Constants from 'expo-constants';
+import NavigationFood from './components/NavigationFood';
+import NavigationNotes from './components/NavigationNotes';
+import NavigationProfile from './components/NavigationProfile';
 
+const Tab = createBottomTabNavigator();
 export default function App() {
+  const [state, setState] = useState({ course: [] });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GlobalContext.Provider value={{ state, setState }}>
+      <NavigationContainer>
+        <Tab.Navigator >
+          <Tab.Screen name="notes" component={NavigationNotes} options={{
+             headerShown: false, tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="microsoft-onenote" color={color} size={26} />
+            )
+          }} />
+          <Tab.Screen name="foods" component={NavigationFood} options={{
+            headerShown: false, tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="food-apple" color={color} size={26} />
+            )
+          }} />
+          <Tab.Screen name="profile" component={NavigationProfile} options={{
+            headerShown: false, tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="information" color={color} size={26} />
+            )
+          }} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </GlobalContext.Provider>
   );
 }
 
@@ -14,7 +44,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
   },
 });

2a6d0d9316f08969fb4abe4302665899fe9085b7
Wengelh <106118460+Wengelh@users.noreply.github.com>
2023-10-23 20:39:00 -0500

    new

--- backend/app.js
+++ backend/app.js
@@ -25,175 +25,6 @@ app.use(cors());
 
 connectDB();
 
-//----------------------------------------------
-// Start of Rajeev Api for Notes
-//---------------------------------------------
-
-// Add Notes
-app.put("/notes/:userEmail", async (req, res) => {
-  try {
-    req.body._id = new ObjectId();
-    const ret = await db
-      .collection(COLLECTION_NAME)
-      .updateOne(
-        { email: req.params.userEmail },
-        { $push: { notes: req.body } }
-      );
-    const getData = await db
-      .collection(COLLECTION_NAME)
-      .findOne({ email: req.params.userEmail });
-    res.status(200).send({ success: true, data: getData.notes });
-  } catch (err) {
-    res.status(500).send({ success: false, err: "DB error" });
-  }
-});
-
-// Get Notes
-app.get("/notes/:userEmail", async (req, res) => {
-  try {
-    const ret = await db
-      .collection(COLLECTION_NAME)
-      .findOne({ email: req.params.userEmail });
-    res.status(200).send({ success: true, data: ret.notes });
-  } catch (err) {
-    res.status(500).send({ success: false, err: "DB error" });
-  }
-});
-
-// Edit Notes
-app.patch("/notes/:userEmail/note/:noteID", async (req, res) => {
-  try {
-    console.log("req.body.title", req.body);
-    const ret = await db.collection(COLLECTION_NAME).updateOne(
-      {
-        email: req.params.userEmail,
-        "notes._id": new ObjectId(req.params.noteID),
-      },
-      {
-        $set: {
-          "notes.$.title": req.body.title,
-          "notes.$.comment": req.body.comment,
-        },
-      }
-    );
-    const getData = await db
-      .collection(COLLECTION_NAME)
-      .findOne({ email: req.params.userEmail });
-    res.status(200).send({ success: true, data: getData.notes });
-  } catch (err) {
-    res.status(500).send({ success: false, err: "DB error" });
-  }
-});
-//---------------------------------
-// End of Rajeev api for Notes
-//------------------------------------
-
-//----------------------------------------------------------------------------
-// Rahel api for Foods
-//-------------------------------------------------------------------------------
-
-app.post("/restaurants", async (req, res) => {
-  try {
-    const user = req.body;
-    const result = await collection.insertOne(user);
-    res.status(200).send({ success: true, data: result });
-  } catch (error) {
-    console.error("Error creating a new restaurant:", error);
-    res
-      .status(500)
-      .send({ success: false, error: "Can not create a new restaurant" });
-  }
-});
-
-app.put("/restaurants/:restaurantId/foods", async (req, res) => {
-  try {
-    const food = req.body;
-    food._id = new ObjectId();
-    const restaurantId = req.params.restaurantId; // Corrected the parameter name
-
-    const result = await collection.updateOne(
-      { _id: new ObjectId(restaurantId) }, // Corrected the filter
-      { $push: { foods: food } }
-    );
-
-    if (result.matchedCount > 0) {
-      // Check if any documents were matched and modified
-      res.status(200).send({ success: true, data: result });
-    } else {
-      res.status(404).send({ success: false, error: "Restaurant not found" });
-    }
-  } catch (error) {
-    res.status(500).send({ success: false, error: "Server error" });
-  }
-});
-
-app.patch("/restaurants/:restaurantId/foods/:foodId", async (req, res) => {
-  try {
-    const { name, origin, price, date, quantity } = req.body;
-    const restaurantId = req.params.restaurantId;
-    const foodId = req.params.foodId;
-    const result = await collection.updateOne(
-      {
-        _id: new ObjectId(restaurantId),
-        "foods._id": new ObjectId(foodId),
-      },
-      {
-        $set: {
-          "foods.$.name": name,
-          "foods.$.origin": origin,
-          "foods.$.price": price,
-          "foods.$.quantity": quantity,
-          "foods.$.date": date,
-        },
-      }
-    );
-    res.status(200).send({ success: true, data: result });
-  } catch (error) {
-    res.status(500).json({ success: false, error: "Server error" });
-  }
-});
-
-app.delete("/restaurants/:restaurantId/foods/:foodId", async (req, res) => {
-  try {
-    const result = await collection.updateOne(
-      {
-        _id: new ObjectId(req.params.restaurantId),
-      },
-      { $pull: { foods: { _id: new ObjectId(req.params.foodId) } } }
-    );
-
-    res.status(200).send({ success: true, data: result });
-  } catch (error) {
-    res.status(500).json({ success: false, error: "Server error" });
-  }
-});
-
-app.get("/restaurants/:restaurantId/foods", async (req, res) => {
-  try {
-    const restaurantId = req.params.restaurantId;
-    const user = await collection.findOne({ _id: new ObjectId(restaurantId) });
-
-    if (!user) {
-      return res
-        .status(404)
-        .json({ success: false, error: "restaurant not found" });
-    }
-
-    const foods = user.foods || [];
-
-    res.status(200).json({ success: true, data: foods });
-  } catch (error) {
-    res.status(500).json({ success: false, error: "Server error" });
-  }
-});
-//----------------------------------------------------------------------------
-// end of Rahel api for Foods
-//-------------------------------------------------------------------------------
-
-//----------------------------------------------------------------------------
-// start of Wengel api for Login/Signup/Auth
-//-------------------------------------------------------------------------------
-
 app.post("/signup", async (req, res) => {
   try {
     const body = req.body;
@@ -238,7 +69,16 @@ app.post("/SignIn", async (req, res) => {
           PRIVATE_KEY
         );
 
-        return res.send({ success: true, data: {token, name: currentOwner.name, phone: currentOwner.phone, email: currentOwner.email, address: currentOwner.address }});
+        return res.send({
+          success: true,
+          data: {
+            token,
+            name: currentOwner.name,
+            phone: currentOwner.phone,
+            email: currentOwner.email,
+            address: currentOwner.address,
+          },
+        });
       } else {
         return res.send({ success: false, error: "wrong password" });
       }
@@ -306,19 +146,25 @@ app.put("/updateUser", async (req, res) => {
     if (updatedData.password) {
       updatedData.password = await bycrpt.hash(updatedData.password, 10);
       result = await db
-      .collection(COLLECTION_NAME)
-      .updateOne({ _id: new ObjectId(ownerID) }, { $set: updatedData });
-    }else{
-      result = await db
-      .collection(COLLECTION_NAME)
-      .updateOne({ _id: new ObjectId(ownerID) }, { $set: {name: updatedData.name, phone: updatedData.phone, address: updatedData.address} });
+        .collection(COLLECTION_NAME)
+        .updateOne({ _id: new ObjectId(ownerID) }, { $set: updatedData });
+    } else {
+      result = await db.collection(COLLECTION_NAME).updateOne(
+        { _id: new ObjectId(ownerID) },
+        {
+          $set: {
+            name: updatedData.name,
+            phone: updatedData.phone,
+            address: updatedData.address,
+          },
+        }
+      );
     }
 
-      return res.status(200).send({
-        success: true,
-        data: result,
-      });
-    
+    return res.status(200).send({
+      success: true,
+      data: result,
+    });
   } catch (error) {
     return res.status(500).send({
       success: false,
@@ -330,6 +176,171 @@ app.put("/updateUser", async (req, res) => {
 //--------------------------
 // End of Wengel Api
 //---------------------------
+
+//----------------------------------------------
+// Start of Rajeev Api for Notes
+//---------------------------------------------
+
+// Add Notes
+app.put("/notes/:userEmail", async (req, res) => {
+  try {
+    req.body._id = new ObjectId();
+    const ret = await db
+      .collection(COLLECTION_NAME)
+      .updateOne(
+        { email: req.params.userEmail },
+        { $push: { notes: req.body } }
+      );
+    const getData = await db
+      .collection(COLLECTION_NAME)
+      .findOne({ email: req.params.userEmail });
+    res.status(200).send({ success: true, data: getData.notes });
+  } catch (err) {
+    res.status(500).send({ success: false, err: "DB error" });
+  }
+});
+
+// Get Notes
+app.get("/notes/:userEmail", async (req, res) => {
+  try {
+    const ret = await db
+      .collection(COLLECTION_NAME)
+      .findOne({ email: req.params.userEmail });
+    res.status(200).send({ success: true, data: ret.notes });
+  } catch (err) {
+    res.status(500).send({ success: false, err: "DB error" });
+  }
+});
+
+// Edit Notes
+app.patch("/notes/:userEmail/note/:noteID", async (req, res) => {
+  try {
+    console.log("req.body.title", req.body);
+    const ret = await db.collection(COLLECTION_NAME).updateOne(
+      {
+        email: req.params.userEmail,
+        "notes._id": new ObjectId(req.params.noteID),
+      },
+      {
+        $set: {
+          "notes.$.title": req.body.title,
+          "notes.$.comment": req.body.comment,
+        },
+      }
+    );
+    const getData = await db
+      .collection(COLLECTION_NAME)
+      .findOne({ email: req.params.userEmail });
+    res.status(200).send({ success: true, data: getData.notes });
+  } catch (err) {
+    res.status(500).send({ success: false, err: "DB error" });
+  }
+});
+//---------------------------------
+// End of Rajeev api for Notes
+//------------------------------------
+
+//----------------------------------------------------------------------------
+// Rahel api for Foods
+//-------------------------------------------------------------------------------
+
+app.post("/restaurants/foods", async (req, res) => {
+  const { ownerID } = req.currentuser;
+  try {
+    const food = req.body;
+    const result = await collection(COLLECTION_NAME).updateOne(
+      { _id: new ObjectId(ownerID) },
+      { $push: { foods: { _id: new ObjectId(), ...food } } }
+    );
+    res.status(200).send({ success: true, data: result });
+  } catch (error) {
+    console.error("Error creating a new restaurant:", error);
+    res
+      .status(500)
+      .send({ success: false, error: "Can not create a new restaurant" });
+  }
+});
+
+app.get("/restaurants/foods", async (req, res) => {
+  try {
+    const food = req.body;
+    food._id = new ObjectId();
+    const restaurantId = req.params.restaurantId;
+
+    const result = await collection(COLLECTION_NAME).updateOne(
+      { _id: new ObjectId(restaurantId) },
+      { $push: { foods: food } }
+    );
+
+    if (result.matchedCount > 0) {
+      res.status(200).send({ success: true, data: result });
+    } else {
+      res.status(404).send({ success: false, error: "Restaurant not found" });
+    }
+  } catch (error) {
+    res.status(500).send({ success: false, error: "Server error" });
+  }
+});
+
+app.patch("/restaurants/:restaurantId/foods/:foodId", async (req, res) => {
+  try {
+    const { name, origin, price, date, quantity } = req.body;
+    const restaurantId = req.params.restaurantId;
+    const foodId = req.params.foodId;
+    const result = await collection.updateOne(
+      {
+        _id: new ObjectId(restaurantId),
+        "foods._id": new ObjectId(foodId),
+      },
+      {
+        $set: {
+          "foods.$.name": name,
+          "foods.$.origin": origin,
+          "foods.$.price": price,
+          "foods.$.quantity": quantity,
+          "foods.$.date": date,
+        },
+      }
+    );
+    res.status(200).send({ success: true, data: result });
+  } catch (error) {
+    res.status(500).json({ success: false, error: "Server error" });
+  }
+});
+
+app.delete("/restaurants/:restaurantId/foods/:foodId", async (req, res) => {
+  try {
+    const result = await collection.updateOne(
+      {
+        _id: new ObjectId(req.params.restaurantId),
+      },
+      { $pull: { foods: { _id: new ObjectId(req.params.foodId) } } }
+    );
+
+    res.status(200).send({ success: true, data: result });
+  } catch (error) {
+    res.status(500).json({ success: false, error: "Server error" });
+  }
+});
+
+app.get("/restaurants/foods", async (req, res) => {
+  const { ownerID } = req.currentuser;
+  try {
+    const user = await collection.findOne({ _id: new ObjectId(ownerID) });
+    if (user) {
+      res.status(200).json({ success: true, data: user.foods });
+    }
+  } catch (error) {
+    res.status(500).json({ success: false, error: "Server error" });
+  }
+});
+//----------------------------------------------------------------------------
+// end of Rahel api for Foods
+//-------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------
+// start of Wengel api for Login/Signup/Auth
+//-------------------------------------------------------------------------------
 
 const PORT = 5001;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));

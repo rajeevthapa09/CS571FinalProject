4f1ac99e58964498003470b6d2dd6525d8d33005
Wengelh <106118460+Wengelh@users.noreply.github.com>
2023-10-23 21:38:56 -0500

    new

--- frontend/components/Foods/Food.js
+++ frontend/components/Foods/Food.js
@@ -1,7 +1,13 @@
 import React, { useContext, useState } from "react";
-import { Alert, View, Text, TouchableHighlight } from "react-native";
+import {
+  Alert,
+  View,
+  Text,
+  TouchableHighlight,
+  StyleSheet,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import styles from "../../styles/myStyles";
+
 import { deleteFood } from "../../utils/network";
 import GlobalContext from "../../utils/context";
 
@@ -21,36 +27,89 @@ const Food = ({ food, onRefresh }) => {
   };
 
   return (
-    <View>
-      <View style={styles.row}>
-        <View style={styles.name}>
-          <Text>Name: {name}</Text>
-          <Text>Origin: {origin}</Text>
-          <Text>Price: {price}</Text>
-
-          <Text>Date: {date}</Text>
-          <Text>Image: {image}</Text>
+    <View style={styles.container}>
+      <View style={styles.info}>
+        <View style={styles.infoRow}>
+          <Text style={styles.label}>Name:</Text>
+          <Text style={styles.text}>{name}</Text>
         </View>
-
-        <View style={styles.edges}>
-          <TouchableHighlight
-            onPress={handleEdit}
-            style={styles.button}
-            underlayColor="#5398DC"
-          >
-            <Text style={styles.buttonText}>Edit</Text>
-          </TouchableHighlight>
-          <TouchableHighlight
-            onPress={handleDelete}
-            style={styles.button}
-            underlayColor="#5398DC"
-          >
-            <Text style={styles.buttonText}>Delete</Text>
-          </TouchableHighlight>
+        <View style={styles.infoRow}>
+          <Text style={styles.label}>Origin:</Text>
+          <Text style={styles.text}>{origin}</Text>
         </View>
+        <View style={styles.infoRow}>
+          <Text style={styles.label}>Price:</Text>
+          <Text style={styles.text}>{price}</Text>
+        </View>
+        <View style={styles.infoRow}>
+          <Text style={styles.label}>Date:</Text>
+          <Text style={styles.text}>{date}</Text>
+        </View>
+        <View style={styles.infoRow}>
+          <Text style={styles.label}>Image:</Text>
+          <Text style={styles.text}>{image}</Text>
+        </View>
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <TouchableHighlight
+          onPress={handleEdit}
+          style={styles.button}
+          underlayColor="#5398DC"
+        >
+          <Text style={styles.buttonText}>Edit</Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          onPress={handleDelete}
+          style={styles.button}
+          underlayColor="#FF5733"
+        >
+          <Text style={styles.buttonText}>Delete</Text>
+        </TouchableHighlight>
       </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    padding: 10,
+    margin: 10,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "#ccc",
+  },
+  info: {
+    marginBottom: 10,
+  },
+  infoRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 5,
+  },
+  label: {
+    flex: 1,
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  text: {
+    flex: 3,
+    fontSize: 16,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  button: {
+    backgroundColor: "#5398DC",
+    padding: 10,
+    margin: 5,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "#fff",
+  },
+});
 
 export default Food;
--- frontend/components/Profile/Home.js
+++ frontend/components/Profile/Home.js
@@ -2,40 +2,57 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Text, View } from "react-native";
 import Profile from "./Profile";
 import ProfileNavigator from "../../stackScreen/ProfileStackNav";
-import NavigationNotes from '../../stackScreen/NavigationNotes';
-import FoodsScreen from "../../stackScreen/FoodsScreen"
-import { MaterialCommunityIcons } from 'react-native-vector-icons';
+import NavigationNotes from "../../stackScreen/NavigationNotes";
+import FoodsScreen from "../../stackScreen/FoodsScreen";
+import { MaterialCommunityIcons } from "react-native-vector-icons";
 
 import { createStackNavigator } from "@react-navigation/stack";
 
 const Tab = createBottomTabNavigator();
 
 export default function Home() {
-    return (
-        <Tab.Navigator >
-            
-            <Tab.Screen name="notes" component={NavigationNotes} options={{
-                headerShown: false, tabBarIcon: ({ color }) => (
-                    <MaterialCommunityIcons name="microsoft-onenote" color={color} size={26} />
-                )
-            }} />
-            <Tab.Screen
-                name="foods"
-                component={FoodsScreen}
-                options={{
-                    headerShown: false,
-                    tabBarIcon: ({ color }) => (
-                        <MaterialCommunityIcons name="food-apple" color={color} size={26} />
-                    ),
-                }}
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="foods"
+        component={FoodsScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="food-apple" color={color} size={26} />
+          ),
+        }}
+      />
+
+      <Tab.Screen
+        name="notes"
+        component={NavigationNotes}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="microsoft-onenote"
+              color={color}
+              size={26}
             />
-            <Tab.Screen
-                name="profile"
-                component={ProfileNavigator}
-                options={{ headerShown: false ,tabBarIcon: ({ color }) => (
-                    <MaterialCommunityIcons name="information" color={color} size={26} />
-                )
-            }} />
-        </Tab.Navigator>
-    );
+          ),
+        }}
+      />
+
+      <Tab.Screen
+        name="profile"
+        component={ProfileNavigator}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="information"
+              color={color}
+              size={26}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
 }
